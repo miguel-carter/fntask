27588a03f8548d498aa110951167b0e7789be30f
Miguel <nullcarter@gmail.com>
2022-07-23 11:24:04 -0500

    initial entry portal

--- src/components/EntryPortal.svelte
+++ src/components/EntryPortal.svelte
@@ -1,3 +1,47 @@
-<div>
-    test
+<script>
+    import {authState} from "../stores/auth.js";
+    import Card from "./commons/Card.svelte";
+    import Button from "./commons/Button.svelte";
+</script>
+
+<div class="wrapper">
+        <Card>
+            <p>username</p>
+            <input type="text">
+            <p>password</p>
+            <input type="text">
+            <div class="controls">
+                <Button text="New"/>
+                <Button text="Login"/>
+            </div> 
+        </Card>
+        <Card>
+            <p>username</p>
+            <input type="text">
+            <p>password</p>
+            <input type="text">
+        </Card>
+       
+    
 </div>
+
+<style>
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 250px;
+        background-color: var(--bg-subdue);
+    }
+
+    .controls {
+        display: flex;
+        flex-direction: row;
+        width: 100%;
+        margin-top: 10px;
+        justify-content: space-around;
+    }
+
+
+</style>
---
+++ src/components/commons/Button.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let text
+</script>
+
+<div class="button">
+    {text}
+</div>
+
+<style>
+    .button {
+        color: var(--color-subdue);
+        padding: 5px 5px;
+        text-align: center;
+        border: 1px solid var(--color-subdue);
+        background-color: transparent;
+        font-size: 12px;
+        width: 100%;
+    }
+    .button:hover {
+        color: white
+    }
+</style>
